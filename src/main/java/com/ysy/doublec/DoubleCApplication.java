4d7e1b09664d203df5fc26fe327f94c058a18c9c
shao12138 <1966799809@qq.com>
2022-09-20 12:24:46 +0800

    s

---
+++ src/main/java/com/ysy/doublec/DoubleCApplication.java
@@ -0,0 +1,13 @@
+package com.ysy.doublec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoubleCApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ysy/doublec/DoubleCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysy.doublec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoubleCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
